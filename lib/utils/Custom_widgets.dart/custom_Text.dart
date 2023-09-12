6e696a1baf55cea14a02d1923e059ac524c528ae
prateekp273 <prateekp18012000@gmail.com>
2023-09-12 11:51:24 +0530

    2nd commit

---
+++ lib/utils/Custom_widgets.dart/custom_Text.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final Color? color;
+  final double? fontSize;
+  final int? letterSpacing;
+  final String? headline;
+
+  const CustomText({
