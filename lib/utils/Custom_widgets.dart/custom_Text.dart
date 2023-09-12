9386fc06e1ebccef29e097aa07ba9ae1357ef66e
prateekp273 <prateekp18012000@gmail.com>
2023-09-12 11:54:01 +0530

    3rd commit

--- lib/utils/Custom_widgets.dart/custom_Text.dart
+++ lib/utils/Custom_widgets.dart/custom_Text.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
 
@@ -9,3 +10,38 @@ class CustomText extends StatelessWidget {
   final String? headline;
 
   const CustomText({
+    Key? key,
+    required this.text,
+    this.color,
+    this.fontSize,
+    this.letterSpacing,
+    this.headline,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).textTheme;
+    final headlineStyle = headline == 'headline1'
+        ? theme.headline1!
+        : headline == 'headline2'
+        ? theme.headline2!
+        : headline == 'headline3'
+        ? theme.headline3!
+        : headline == 'headline4'
+        ? theme.headline4!
+        : headline == 'headline5'
+        ? theme.headline5!
+        : headline == 'headline6'
+        ? theme.headline6!
+        : theme.headline1!;
+
+    return Text(
+      text,
+      style: headlineStyle.copyWith(
+        color: color,
+        fontSize: fontSize,
+        letterSpacing: letterSpacing?.toDouble(),
+      ),
+    );
+  }
+}
